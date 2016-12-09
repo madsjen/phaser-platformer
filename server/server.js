3734a683a92001f9446dd3b512a3a0cf491eb4ee
madsjen <madsjensen5@gmail.com>
2016-12-09 11:09:25 +0100

    removed public server folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-
 io.on('connection', (socket) => {
 	console.log('a user connected:')
 })
